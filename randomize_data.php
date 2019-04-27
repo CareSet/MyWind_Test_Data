a645150670976c0dd360911646a54cd555b574e1
Fred Trotter <fred.trotter@gmail.com>
2019-04-27 17:51:41 -0500

    randomize these

---
+++ randomize_data.php
@@ -0,0 +1,63 @@
+<?php
+
+	$randomize_these_fields = [
+		'MyWind_northwind_model' => [
+			'customer' => [
+				'emailAddress' => 'email',
+				'businessPhone' => 'phone',
+				'homePhone' => 'phone',
+				'mobilePhone' => 'phone',
+				'faxNumber' => 'phone',
+				'address' => 'streetAddress',
+				'zipPostalCode' => 'postcode',
+				'webPage' => 'url',
+				'notes' => 'latin',
+				'attachements' => 'latin',
+				'random_date' => 'date',
+				'created_at' => 'date',
+				'updated_at' => 'date',
+			],
+
+			'employee' => [
+				'businessPhone' => 'phone',
+				'homePhone' => 'phone',
+				'mobilePhone' => 'phone',
+				'faxNumber' => 'phone',
+				'address' => 'streetAddress',
+				'zipPostalCode' => 'postcode',
+			],
+			'shipper' = [
+				'lastName' => 'lastName',
+				'firstName' => 'firstName',
+				'emailAddress' => 'email',
+				'jobTitle' => 'jobTitle',
+				'homePhone' => 'phone',
+				'mobilePhone' => 'phone',
+				'faxNumber' => 'phone',
+				'address' => 'streetAddress',
+				'zipPostalCode' => 'postcode',
+				'webPage' => 'url',
+				'notes' => 'latin',
+				'attachements' => 'latin',
+			],
+
+			'supplier' = [
+				'lastName' => 'lastName',
+				'firstName' => 'firstName',
+				'emailAddress' => 'email',
+				'homePhone' => 'phone',
+				'mobilePhone' => 'phone',
+				'faxNumber' => 'phone',
+				'address' => 'streetAddress',
+				'zipPostalCode' => 'postcode',
+				'webPage' => 'url',
+				'notes' => 'latin',
+				'attachements' => 'latin',
+			],
+					
+		],
+
+
+	];
+
+
